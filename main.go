5138ea82c86044880ed26d0a4bcab3bcf543dffa
Pariwat Yimpayoon <32978747+PariwatY@users.noreply.github.com>
2022-04-17 15:37:23 +0700

    learning fiber framework

--- main.go
+++ main.go
@@ -1,1 +1,52 @@
-package gofiberframework
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	//GET
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("hello")
+	})
+
+	//POST
+	app.Post("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("post hello")
+	})
+
+	//Parameter
+	app.Get("/hello/name/:name", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		return c.SendString("name: " + name)
+	})
+
+	//Parameter Optional
+	app.Get("/hello/:name/:surname", func(c *fiber.Ctx) error {
+		name := c.Params("name")
+		surname := c.Params("surname")
+		return c.SendString("name: " + name + " surname: " + surname)
+	})
+
+	//Parameter Integer
+	app.Get("/hello/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil {
+			return fiber.ErrBadRequest
+		}
+		return c.SendString(fmt.Sprintf("id: %v", id))
+	})
+
+	//Query
+	app.Get("/query", func(c *fiber.Ctx) error {
+		name := c.Query("name")
+		surname := c.Query("surname")
+		return c.SendString("name: " + name + ", surname: " + surname)
+	})
+
+	app.Listen(":8081")
+}
